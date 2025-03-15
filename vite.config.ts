659cbea670cde00fa56ad63416af887277b0f3b9
DavidSwider <davidswider6@gmail.com>
2025-03-14 20:48:17 -0400

    added render host

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,6 @@ export default defineConfig({
    server: {
     host: '0.0.0.0', // Bind to 0.0.0.0 to allow external access
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000, // Use PORT environment variable if set, otherwise default to 3000
+     allowedHosts: ['candidatesearch-ndbo.onrender.com'], // Add your Render host here
   },
 });
