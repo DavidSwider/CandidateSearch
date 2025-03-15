b32124a98986a8eceeaf3b59cade8d1bc3db6967
DavidSwider <davidswider6@gmail.com>
2025-03-14 20:42:18 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+   server: {
+    host: '0.0.0.0', // Bind to 0.0.0.0 to allow external access
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000, // Use PORT environment variable if set, otherwise default to 3000
+  },
 });
